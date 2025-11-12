f950a89b8f176aeca1092af38b64671be08536f7
UdhayKrishn <krishnauday320@gmail.com>
2025-11-13 00:44:01 +0530

    feat: added redis module in app modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,14 @@
 import { MongoDbModule } from "./mongodb.module";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { RedisModule } from "./redis.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
+		RedisModule,
 		MongoDbModule,
 	],
 })
