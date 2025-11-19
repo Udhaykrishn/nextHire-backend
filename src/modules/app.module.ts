5a2a5598e99e89850beaaac766e4eab0fef5e04b
UdhayKrishn <krishnauday320@gmail.com>
2025-11-19 21:53:24 +0530

    feat: Add Docker Compose for Prometheus and Grafana
    **Short summary:** Add Docker Compose for Prometheus and Grafana

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from "@nestjs/config";
 import { RedisModule } from "./redis.module";
 import { UserModule } from "./user";
 import { AuthModule } from "./auth.module";
+// import { OTelModule } from "./otel.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
+		// OTelModule,
 		RedisModule,
 		MongoDbModule.forRootAsync(),
 		//  EmailQueueModule,
---
+++ src/otel.ts
@@ -0,0 +1,29 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+const prometheusExporter = new PrometheusExporter(
+  {
+    port: 9464,
+    endpoint: "/metrics",
+  },
+  () => {
+    console.log("✔ Prometheus metrics ready at http://localhost:9464/metrics");
+  }
+);
+
+export const otelSDK = new NodeSDK({
+
+  metricReader: prometheusExporter,
+
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-http": {
+        enabled: true,
+      },
+      "@opentelemetry/instrumentation-express": {
+        enabled: true,
+      },
+    }),
+  ],
+});
