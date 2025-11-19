37572e4a0b61f19e7a91b34b202cd54de7eaade9
UdhayKrishn <krishnauday320@gmail.com>
2025-11-19 21:53:24 +0530

    feat: Add Docker Compose for Prometheus and Grafana
    **Short summary:** Add Docker Compose for Prometheus and Grafana

---
+++ src/modules/otel.module.ts
@@ -0,0 +1,20 @@
+// import { Module } from '@nestjs/common';
+// import { OpenTelemetryModule } from 'nestjs-otel';
+
+// @Module({
+//   imports: [
+//     OpenTelemetryModule.forRoot({
+//       metrics: {
+//         hostMetrics: true,
+//         apiMetrics: {
+//           enable: true,
+//           defaultAttributes: { custom: 'label' },
+//           ignoreRoutes: ['/favicon.ico'],
+//           ignoreUndefinedRoutes: false,
+//           prefix: 'my_prefix',
+//         },
+//       },
+//     }),
+//   ],
+// })
+// export class OTelModule {}
