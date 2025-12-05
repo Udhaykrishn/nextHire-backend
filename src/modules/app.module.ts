999f820211147bc9e08adc2d568f06fe404620b9
UdhayKrishn <krishnauday320@gmail.com>
2025-12-05 14:08:58 +0530

    feat(modules): integrate upload module into app module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,35 +3,35 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { RedisModule } from "./redis.module";
 import { UserModule } from "./user";
-import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
-import { APP_GUARD } from "@nestjs/core";
 import { RecruiterModule } from "./recruiter/recruiter.module";
 import { AuthModule } from "./auth/auth.module";
+import { UploadModule } from "./upload/upload.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
-		ThrottlerModule.forRoot([
-			{
-				name: "short",
-				ttl: 60000,
-				limit: 10,
-			},
-		]),
+		// ThrottlerModule.forRoot([
+		// 	{
+		// 		name: "short",
+		// 		ttl: 60000,
+		// 		limit: 10,
+		// 	},
+		// ]),
 		RedisModule,
 		MongoDbModule.forRootAsync(),
 		//  EmailQueueModule,
 		AuthModule,
 		UserModule,
 		RecruiterModule,
+		UploadModule,
 	],
 	providers: [
-		{
-			provide: APP_GUARD,
-			useClass: ThrottlerGuard,
-		},
+		// {
+		// 	provide: APP_GUARD,
+		// 	useClass: ThrottlerGuard,
+		// },
 	],
 })
-export class AppModule {}
+export class AppModule { }
