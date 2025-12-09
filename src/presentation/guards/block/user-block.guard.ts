6143165579a86a4c7f4080b95a038442bf8fe46d
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:47:36 +0530

    feat(user): update user block guard

--- src/presentation/guards/block/user-block.guard.ts
+++ src/presentation/guards/block/user-block.guard.ts
@@ -1,17 +1,25 @@
 import { USERS_TOKEN } from "@/application/enums/tokens";
 import type { IExecutable } from "@/application/interface/executable.interface";
 import { ROLES } from "@/presentation/enums";
-import { CanActivate, ExecutionContext, ForbiddenException, Inject, Injectable } from "@nestjs/common";
+
+import {
+	CanActivate,
+	ExecutionContext,
+	ForbiddenException,
+	Inject,
+	Injectable,
+} from "@nestjs/common";
 
 @Injectable()
 export class UserBlockedGuard implements CanActivate {
 	constructor(
 		@Inject(USERS_TOKEN.CHECK_USER_BLOCKED_USE_CASE)
 		private readonly checkUserBlocked: IExecutable<string, void>,
-	) {}
+	) { }
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const req = context.switchToHttp().getRequest();
+
 		const payload = req.user;
 
 		if (payload?.role !== ROLES.USER || !payload?.id) return true;
@@ -19,7 +27,9 @@ export class UserBlockedGuard implements CanActivate {
 		try {
 			await this.checkUserBlocked.execute(payload.id);
 			return true;
+
 		} catch (error) {
+
 			throw new ForbiddenException(error.message);
 		}
 	}
