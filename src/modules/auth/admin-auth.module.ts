6803e53cced68f3728ff5048e18659066484a4b0
UdhayKrishn <krishnauday320@gmail.com>
2025-12-09 16:47:36 +0530

    feat(admin): add admin auth module

---
+++ src/modules/auth/admin-auth.module.ts
@@ -0,0 +1,35 @@
+import { Module } from "@nestjs/common";
+import { AdminLiteModule } from "../admin/admin-db.module";
+import { CommonModule } from "../common.module";
+import { AdminAuthController } from "@/presentation/controller/admin/admin-auth.controller";
+import { ADMIN_AUTH_TOKEN, COMMON_TOKEN } from "@/application/enums/tokens";
+import {
+    AdminLoginUseCase,
+    AdminLogoutUseCase,
+    AdminRefreshUseCase,
+} from "@/application/use-case/auth/admin";
+import { JwtService } from "@/infrastructure/services/implements";
+
+@Module({
+    imports: [AdminLiteModule, CommonModule],
+    controllers: [AdminAuthController],
+    providers: [
+        {
+            provide: ADMIN_AUTH_TOKEN.ADMIN_LOGIN_USE_CASE,
+            useClass: AdminLoginUseCase,
+        },
+        {
+            provide: ADMIN_AUTH_TOKEN.ADMIN_REFRESH_USE_CASE,
+            useClass: AdminRefreshUseCase,
+        },
+        {
+            provide: ADMIN_AUTH_TOKEN.ADMIN_LOGOUT_USE_CASE,
+            useClass: AdminLogoutUseCase,
+        },
+        {
+            provide: COMMON_TOKEN.JWT_SERVICE,
+            useClass: JwtService,
+        },
+    ],
+})
+export class AdminAuthModule { }
