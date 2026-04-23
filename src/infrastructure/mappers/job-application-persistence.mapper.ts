c3e34dbb34db445d5bc79404266f41474ea56f41
uday <krishnauday320@gmail.com>
2026-04-23 17:36:32 +0530

    feat(application): implement application logic, mapper, and token

---
+++ src/infrastructure/mappers/job-application-persistence.mapper.ts
@@ -0,0 +1,31 @@
+import type { IJobApplicationPersistenceMapper } from "@/application/interface/mappers/job/job-application-persistence-mapper.interface";
+import { JobApplicationEntity, APPLICATION_STATUS } from "@/domain/entity/job-application.entity";
+import type { ApplicationType } from "../db/mongodb/models/application.schema";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class JobApplicationPersistenceMapper
+	implements IJobApplicationPersistenceMapper<JobApplicationEntity, ApplicationType>
+{
+	toMongo(data: JobApplicationEntity): ApplicationType {
+		return {
+			_id: data.id as string,
+			userId: data.userId,
+			jobId: data.jobId,
+			status: data.status,
+			createdAt: data.createdAt,
+			updatedAt: data.updatedAt,
+		};
+	}
+
+	async fromMongo(data: ApplicationType): Promise<JobApplicationEntity> {
+		return JobApplicationEntity.create({
+			id: data._id.toString(),
+			userId: data.userId.toString(),
+			jobId: data.jobId.toString(),
+			status: data.status as APPLICATION_STATUS,
+			createdAt: data.createdAt,
+			updatedAt: data.updatedAt,
+		});
+	}
+}
---
+++ src/presentation/enums/job-router.enum.ts
@@ -0,0 +1,6 @@
+export enum JOB_ROUTERS {
+	ROUTER = "job",
+	DEFAULT = "/",
+	APPLY = "/apply",
+	ID_PARAM = "jobId",
+}
